bc3aaf246eeafc9fed349702c350557651ae4645
refaldy04 <refaldybanggana@gmail.com>
2022-08-18 11:50:29 +0700

    setup

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,6 +1,5 @@
 const auth = require("express").Router();
 const authController = require("../controllers/auth");
-const { body } = require("express-validator");
 
 auth.post("/register", authController.register);
 auth.post("/login", authController.login);
